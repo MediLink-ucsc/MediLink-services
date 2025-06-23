d7e1a75629ce01a0edd80c731a3d20bda4718381
Hansaja Kithmal Damsara <hansajak@orelit.com>
2025-06-24 04:57:09 +0530

    Initial file structure with packages

---
+++ services/api-gateway/src/config/logger.ts
@@ -0,0 +1,6 @@
+import { config } from '.';
+import { getLogger } from '@medilink/logger';
+
+const logger = getLogger(config.SERVICE_NAME, config.LOG_LEVEL);
+
+export default logger;
---
+++ services/api-gateway/src/config/redis.ts
@@ -0,0 +1,4 @@
+import RedisClient from '@medilink/redis-client';
+import { config } from '.';
+
+export const redisClient = new RedisClient(config.REDIS_URL);
